4d709a42355f7c07537649464c5e0aa33f3e7e6c
mrGreen <drlvitalii@gmail.com>
2017-12-03 16:00:41 +0200

    works with subImages algorithm. Needs to fix pixels compare

--- src/com/ScreenUtils.java
+++ src/com/ScreenUtils.java
@@ -10,6 +10,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+* 1. Загрузить две картинки картинки
+* 2. Разбить картинку на зоны (прямоугольники)
+* 3. Создать массив соответствующий зонам картинки
+* 4. Пройтись по  зонам  маленького массива и сравнить попиксельно каждую зону,
+*       пометить зону если внутреннее содержимое отличается больше чем на указанный коефициент
+* 5. Пройтись по маленькому помеченному массиву, промаркировать зоны
+* 6. Пройтись по маркированным зонам, забрать крайние координаты для отрисовки прямоугольников
+* 7. Сохранить результат работы в картинку.
+ *
+* */
 
 public class ScreenUtils {
     private static BufferedImage image1;
@@ -21,30 +32,159 @@ public class ScreenUtils {
     private static String path2 = absolutePath + "\\image2.png";
     private static String resultPath = absolutePath + "\\image3.png";
 
-    public static final int NEAREST_PIXEL_GAP = 3;
+    public static final int NEAREST_PIXEL_GAP = 1;
     private static int zoneMarkerCounter = 1;
     private static double differenceKoef = 0.1;
 
+    private static int xBlocksAmount = 40;
+    private static int yBlocksAmount = 40;
+    private static int subImageWidth;
+    private static int subImageHeight;
+
     public static void main(String[] args) {
-        ScreenUtils sc = new ScreenUtils();
+
         try {
-            sc.image1 = downloadImage(path1);
-            sc.image2 = downloadImage(path2);
-            sc.imageResult = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_RGB);
-            int[][] imageDifferenceArray = makeArrayWithDifferentCells(sc.image1, sc.image2);
+            image1 = downloadImage(path1);
+            image2 = downloadImage(path2);
+            imageResult = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+            int[][] imageDifferenceArray = makeSubImageArrayWithDifferentCells(image1, image2);
 //            printArray(imageDifferenceArray);
+//            System.err.println("-----------------");
             zoneMarkerCounter = markDifferentSections(imageDifferenceArray, zoneMarkerCounter);
+//            printArray(imageDifferenceArray);
             List<Zone> zoneList = new ArrayList<>();
             for (int i = 1; i < zoneMarkerCounter; i++) {
-                Zone rec = getRectangeCoordinates(imageDifferenceArray, i);
+                Zone rec = getRectangleCoordinates(imageDifferenceArray, i);
                 zoneList.add(rec);
             }
             drawRectangle(imageResult, zoneList);
             saveImage(imageResult, resultPath);
 
-
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static int[][] makeSubImageArrayWithDifferentCells(BufferedImage image1, BufferedImage image2) {
+        int[][] subImagesArray = new int[yBlocksAmount][xBlocksAmount];
+        subImageWidth = image1.getWidth() / xBlocksAmount;
+        subImageHeight = image1.getHeight() / yBlocksAmount;
+
+//        System.out.println("width: " + image1.getWidth());
+//        System.out.println("height: " + image1.getHeight());
+//        System.out.println("subImageWidth: " + subImageWidth);
+//        System.out.println("subImageHeight: " + subImageHeight);
+//        System.out.println("xBlocksAmount: " + xBlocksAmount);
+//        System.out.println("yBlocksAmount: " + yBlocksAmount);
+
+        for (int y = 0; y < yBlocksAmount; y++) {
+            for (int x = 0; x < xBlocksAmount; x++) {
+                int subImage1[][] = getSubImageArray(image1.getSubimage(x * subImageWidth, y * subImageHeight, subImageWidth - 1, subImageHeight - 1));
+                int subImage2[][] = getSubImageArray(image2.getSubimage(x * subImageWidth, y * subImageHeight, subImageWidth - 1, subImageHeight - 1));
+
+                if (isSubImagesDifferent(subImage1, subImage2)) {
+                    subImagesArray[y][x] = -1;
+//                    System.out.println("  y-> " + (y) + " x-> " + (x));
+                }
+            }
+        }
+        return subImagesArray;
+    }
+
+    public static boolean isSubImagesDifferent(int[][] subImage1, int[][] subImage2) {
+        for (int i = 0; i < subImage1.length; i++) {
+            for (int j = 0; j < subImage1[0].length; j++) {
+//                int diff = Math.abs(subImage1[i][j] - subImage2[i][j]);
+//                if (diff / Math.abs(subImage1[i][j]) > differenceKoef) {
+//                    return true;
+//                }
+                if (subImage1[i][j] != subImage2[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[][] getSubImageArray(BufferedImage subImage) {
+        int width = subImage.getWidth();
+        int height = subImage.getHeight();
+        int[][] subImageArray = new int[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                subImageArray[row][col] = subImage.getRGB(col, row);
+            }
+        }
+        return subImageArray;
+    }
+
+    public static Zone getRectangleCoordinates(int[][] ar, int region) {
+        Zone reg = new Zone();
+        reg.regionNumber = region;
+        boolean isFirstCoordSet = true;
+        for (int y = 0; y < ar.length; y++) {
+            for (int x = 0; x < ar[y].length; x++) {
+                if (ar[y][x] == reg.regionNumber) {
+                    if (isFirstCoordSet) {
+                        reg.startX = x * subImageWidth;
+                        reg.startY = y * subImageHeight;
+                        reg.endX = x * subImageWidth + (subImageWidth - 1);
+                        reg.endY = y * subImageHeight + (subImageHeight - 1);
+                        isFirstCoordSet = false;
+                    }
+                    int x1 = x * subImageWidth;
+                    int y1 = y * subImageHeight;
+                    reg.startX = (x1 < reg.startX ? x1 : reg.startX);
+                    reg.startY = (y1 < reg.startY ? y1 : reg.startY);
+                    reg.endX = (x1 + (subImageWidth - 1) > reg.endX ? x1 + (subImageWidth - 1) : reg.endX);
+                    reg.endY = (y1 + (subImageHeight - 1) > reg.endY ? y1 + (subImageHeight - 1) : reg.endY);
+                }
+            }
+        }
+        return reg;
+    }
+
+    public static int markDifferentSections(int[][] array, int zoneMarker) {
+        for (int y = 0; y < array.length; y++) {
+            for (int x = 0; x < array[y].length; x++) {
+                if (array[y][x] == -1) {
+                    recur(array, y, x, zoneMarker);
+                    zoneMarker++;
+                }
+            }
+        }
+        return zoneMarker;
+    }
+
+    public static void recur(int[][] array, int i, int j, int zoneMarker) {
+        try {
+            array[i][j] = zoneMarker;
+            for (int y = i - NEAREST_PIXEL_GAP; y <= i + NEAREST_PIXEL_GAP; y++) {
+                for (int x = j - NEAREST_PIXEL_GAP; x <= j + NEAREST_PIXEL_GAP; x++) {
+                    if (array[y][x] == -1) {
+                        recur(array, y, x, zoneMarker);
+                    }
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+    }
+
+    public static void printArray(int[][] ar) {
+
+        for (int y = 0; y < ar.length; y++) {
+            for (int x = 0; x < ar[y].length; x++) {
+                if (ar[y][x] == -1) {
+                    System.out.print("+");
+                } else if (ar[y][x] == 0) {
+                    System.out.print(ar[y][x]);
+                } else {
+                    System.out.print(ar[y][x]);
+                }
+            }
+            System.out.println();
         }
     }
 
@@ -55,7 +195,6 @@ public class ScreenUtils {
         for (Zone rec : zoneList) {
             g2.drawRect(rec.startX, rec.startY, rec.endX - rec.startX, rec.endY - rec.startY);
         }
-
     }
 
     public static BufferedImage downloadImage(String filename) throws IOException {
@@ -75,93 +214,6 @@ public class ScreenUtils {
             ImageIO.write(bimg, "png", outPutFile);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static int[][] makeArrayWithDifferentCells(BufferedImage image1, BufferedImage image2) {
-        int width = image1.getWidth();
-        int height = image1.getHeight();
-        int resultArray[][] = new int[height][width];
-
-        for (int i = 0; i < height - 2; i++) {
-            for (int j = 0; j < width - 2; j++) {
-                double difference = (Math.abs(image1.getRGB(j, i) - image2.getRGB(j, i)));
-                double factDifKoef = difference / Math.abs(image1.getRGB(j, i));
-                if (factDifKoef > differenceKoef) {
-                    resultArray[i][j] = -1;
-                } else {
-                    resultArray[i][j] = 0;
-                }
-            }
-        }
-        return resultArray;
-    }
-
-    public static Zone getRectangeCoordinates(int[][] ar, int region) {
-        Zone reg = new Zone();
-        reg.regionNumber = region;
-        boolean isFirstCoordSet = true;
-        for (int i = 0; i < ar.length; i++) {
-            for (int j = 0; j < ar[i].length; j++) {
-                if (ar[i][j] == reg.regionNumber) {
-                    if (isFirstCoordSet) {
-                        reg.startX = j;
-                        reg.startY = i;
-                        reg.endX = j;
-                        reg.endY = i;
-                        isFirstCoordSet = false;
-                    }
-                    reg.startX = (j < reg.startX ? j : reg.startX);
-                    reg.startY = (i < reg.startY ? i : reg.startY);
-                    reg.endX = (j > reg.endX ? j : reg.endX);
-                    reg.endY = (i > reg.endY ? i : reg.endY);
-                }
-            }
-        }
-        return reg;
-    }
-
-    public static int markDifferentSections(int[][] array, int zoneMarker) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] == -1) {
-                    recur(array, i, j, zoneMarker);
-                    zoneMarker++;
-                }
-            }
-        }
-        return zoneMarker;
-    }
-
-    public static void recur(int[][] array, int i, int j, int zoneMarker) {
-        try {
-            array[i][j] = zoneMarker;
-            for (int k = i - NEAREST_PIXEL_GAP; k <= i + NEAREST_PIXEL_GAP; k++) {
-                for (int l = j - NEAREST_PIXEL_GAP; l <= j + NEAREST_PIXEL_GAP; l++) {
-                    if (array[k][l] == -1) {
-                        recur(array, k, l, zoneMarker);
-                    }
-                }
-            }
-            return;
-        } catch (IndexOutOfBoundsException e) {
-
-        }
-    }
-
-    public static void printArray(int[][] ar) {
-
-        for (int i = 0; i < ar.length; i++) {
-            for (int j = 0; j < ar[i].length; j++) {
-                if (ar[i][j] == -1) {
-                    System.out.print("+");
-                } else if (ar[i][j] == 0) {
-                    System.out.print(ar[i][j]);
-                } else {
-                    System.out.print(ar[i][j]);
-                }
-            }
-            System.out.println();
         }
     }
 }
