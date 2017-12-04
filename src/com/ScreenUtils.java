8f69b003f86e9407a0e2a68984261715cf563a4b
mrGreen <drlvitalii@gmail.com>
2017-12-04 10:54:03 +0200

    added UI for the program.

--- src/com/ScreenUtils.java
+++ src/com/ScreenUtils.java
@@ -1,5 +1,16 @@
 package com;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 import javax.imageio.ImageIO;
 
 
@@ -11,26 +22,103 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-* 1. Загрузить две картинки картинки
-* 2. Разбить картинку на зоны (прямоугольники)
-* 3. Создать массив соответствующий зонам картинки
-* 4. Пройтись по  зонам  маленького массива и сравнить попиксельно каждую зону,
-*       пометить зону если внутреннее содержимое отличается больше чем на указанный коефициент
-* 5. Пройтись по маленькому помеченному массиву, промаркировать зоны
-* 6. Пройтись по маркированным зонам, забрать крайние координаты для отрисовки прямоугольников
-* 7. Сохранить результат работы в картинку.
- *
+* 1. Download images
+* 2. Split image to rectangles
+* 3. Create an array according rectangles
+* 4. Iterate over an rectArray and compare each rectangle with pixels.
+*      Mark rectArray cell if rectangle contests are different more than koef.
+* 5. Iterate over an recArray and take numbers for marked cells.
+* 6. Iterate over an marked array and get coordinates for ther rectangles.
+* 7. Save the result.
 * */
 
 public class ScreenUtils {
+
+    @FXML
+    Button imageChooser1;
+
+    @FXML
+    Button imageChooser2;
+
+    @FXML
+    Button folderChooser;
+
+    @FXML
+    Button compare;
+
+    @FXML
+    Label imageLbl1;
+
+    @FXML
+    Label imageLbl2;
+
+    @FXML
+    Label imageLbl3;
+
+    @FXML
+    Label totalTime;
+
+    @FXML
+    ImageView imgV1;
+
+    @FXML
+    ImageView imgV2;
+
+    @FXML
+    ImageView imgV3;
+
+    public void imgChoser1(ActionEvent event) {
+        FileChooser fc1 = new FileChooser();
+        File file1 = fc1.showOpenDialog(null);
+        if (file1 != null) {
+            imageLbl1.setText(file1.getAbsolutePath());
+            imgV1.setImage(new Image(file1.getAbsoluteFile().toURI().toString()));
+            path1 = file1.getAbsolutePath();
+        } else {
+            System.out.println("file is not valid");
+        }
+    }
+
+    public void imgChoser2(ActionEvent event) {
+        FileChooser fc2 = new FileChooser();
+        File file2 = fc2.showOpenDialog(null);
+        if (file2 != null) {
+            imageLbl2.setText(file2.getAbsolutePath());
+            imgV2.setImage(new Image(file2.getAbsoluteFile().toURI().toString()));
+            path2 = file2.getAbsolutePath();
+        } else {
+            System.out.println("file is not valid");
+        }
+    }
+
+
+    public void fileSaveChooser(ActionEvent event) {
+        FileChooser fc3 = new FileChooser();
+        fc3.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        File directory = fc3.showSaveDialog(null);
+        if (directory != null) {
+            imageLbl3.setText(directory.getAbsolutePath());
+            resultPath = directory.getAbsolutePath();
+        } else {
+            System.out.println("file is not valid");
+        }
+    }
+
     private static BufferedImage image1;
     private static BufferedImage image2;
     private static BufferedImage imageResult;
 
-    private static String absolutePath = "F:";
-    private static String path1 = absolutePath + "\\image1.png";
-    private static String path2 = absolutePath + "\\image2.png";
-    private static String resultPath = absolutePath + "\\image3.png";
+    //FOR DEBUG
+//    private static String absolutePath = "F:";
+//    private static String path1 = absolutePath + "\\image1.png";
+//    private static String path2 = absolutePath + "\\image2.png";
+//    private static String resultPath = absolutePath + "\\image3.png";
+
+    private static String path1;
+    private static String path2;
+    private static String resultPath;
 
     public static final int NEAREST_PIXEL_GAP = 1;
     private static int zoneMarkerCounter = 1;
@@ -42,17 +130,21 @@ public class ScreenUtils {
     private static int subImageHeight;
 
     public static void main(String[] args) {
+//        ScreenUtils sc = new ScreenUtils();
+//        sc.compare();
+    }
 
+    //TODO make possible to compare images with different size
+    public void compare() {
         try {
             image1 = downloadImage(path1);
             image2 = downloadImage(path2);
             imageResult = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_RGB);
-
+            long startTime = System.currentTimeMillis();
             int[][] imageDifferenceArray = makeSubImageArrayWithDifferentCells(image1, image2);
-//            printArray(imageDifferenceArray);
-//            System.err.println("-----------------");
+//            printArray(imageDifferenceArray);//DEBUG
             zoneMarkerCounter = markDifferentSections(imageDifferenceArray, zoneMarkerCounter);
-//            printArray(imageDifferenceArray);
+//            printArray(imageDifferenceArray);//DEBUG
             List<Zone> zoneList = new ArrayList<>();
             for (int i = 1; i < zoneMarkerCounter; i++) {
                 Zone rec = getRectangleCoordinates(imageDifferenceArray, i);
@@ -60,6 +152,10 @@ public class ScreenUtils {
             }
             drawRectangle(imageResult, zoneList);
             saveImage(imageResult, resultPath);
+            File tempFile = new File(resultPath);
+            imgV3.setImage(new Image(tempFile.toURI().toString()));
+            long endTime = System.currentTimeMillis();
+            totalTime.setText("Total time: " + (endTime - startTime) + " ms");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,6 +167,7 @@ public class ScreenUtils {
         subImageWidth = image1.getWidth() / xBlocksAmount;
         subImageHeight = image1.getHeight() / yBlocksAmount;
 
+//DEBUG
 //        System.out.println("width: " + image1.getWidth());
 //        System.out.println("height: " + image1.getHeight());
 //        System.out.println("subImageWidth: " + subImageWidth);
@@ -85,7 +182,7 @@ public class ScreenUtils {
 
                 if (isSubImagesDifferent(subImage1, subImage2)) {
                     subImagesArray[y][x] = -1;
-//                    System.out.println("  y-> " + (y) + " x-> " + (x));
+//                    System.out.println("  y-> " + (y) + " x-> " + (x));//DEBUG
                 }
             }
         }
@@ -95,6 +192,7 @@ public class ScreenUtils {
     public static boolean isSubImagesDifferent(int[][] subImage1, int[][] subImage2) {
         for (int i = 0; i < subImage1.length; i++) {
             for (int j = 0; j < subImage1[0].length; j++) {
+                //TODO FIX PIXELS COMPARE
 //                int diff = Math.abs(subImage1[i][j] - subImage2[i][j]);
 //                if (diff / Math.abs(subImage1[i][j]) > differenceKoef) {
 //                    return true;
@@ -173,7 +271,6 @@ public class ScreenUtils {
     }
 
     public static void printArray(int[][] ar) {
-
         for (int y = 0; y < ar.length; y++) {
             for (int x = 0; x < ar[y].length; x++) {
                 if (ar[y][x] == -1) {
@@ -216,4 +313,6 @@ public class ScreenUtils {
             e.printStackTrace();
         }
     }
+
+
 }
